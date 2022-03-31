8ad40034e01f031b72d7133c72957cf262e40b81
EmilPaizano <pabloemilio.beta@gmail.com>
2022-03-30 21:25:38 -0600

    Primera Parte EntregarTareas

---
+++ src/Lib/apiClient.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+//AQUI CREAMOS 2 INSTANCIAS, UNA PARA PETICIONES PUBLICAS Y UNA PARA PETICIONES PRIVADAS
+export const publicAxios = axios.create();
+export const privateAxios = axios.create();
+
+//DEFINIMOS LA EL VALOR DEL APITOKEN EN AXIOS, DE IGUAL FORMA PARA PUBLICAS Y PRIVADAS,
+//EXISTE UNA CONDICION QUE DICE QUE CADA VARIABLE DE ENTORNO EN .env DEBE INICIAR EN MAYUSCULAS
+//COM "REACT_<NOMBRE_VARIABLE_ENTORNO>"
+publicAxios.defaults.headers.common['apitoken'] = process.env.REACT_APP_APITOKEN;
+privateAxios.defaults.headers.common['apitoken'] = process.env.REACT_APP_APITOKEN;
+
+//CREAMOS LA VARIABLE DE ENTORNO CON y se la pasamos a axios baseURL 
+publicAxios.defaults.baseURL = process.env.REACT_APP_APIURL;
+privateAxios.defaults.baseURL = process.env.REACT_APP_APIURL;
+
+//QUITAR EL CACHE PARA QUE NO INTERFIERA
+publicAxios.defaults.headers.common['cache-contro'] = 'no-cache'
+privateAxios.defaults.headers.common['cache-contro'] = 'no-cache'
+
+
+publicAxios.defaults.headers.common['Content-Type'] = 'application/json'
+privateAxios.defaults.headers.common['Content-Type'] = 'application/json'
+
+// export const setJWT = ( jwt ) =>{
+//     privateAxios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
+// }
