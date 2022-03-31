668c5c40226ccaa34857ebdd0a9abcb05509fbc2
EmilPaizano <pabloemilio.beta@gmail.com>
2022-03-31 14:52:19 -0600

    CRUD funcional en Entrega de Tareas

--- src/components/screens/Tareas/EntregarTareas/EntregarTareas.js
+++ src/components/screens/Tareas/EntregarTareas/EntregarTareas.js
@@ -3,16 +3,36 @@ import "./EntregarTareas.css";
 import { privateAxios, publicAxios } from "../../../../lib/apiClient";
 
 const Entregar =()=>{
-    const id = "6225659f6208d12d1cdce801"
+    const idClase = "6225659f6208d12d1cdce801"
+    const idEstudiante = "622541aef1bd3418216d8831"
+    const numTarea = "HW17_3_638" //HW17_3_902 //HW17_3_638
+
     const [descripcion, setDescripcion] = useState("");
     const [titulo, setTitulo] = useState("");
     const [puntaje, setPuntaje] = useState("");
     const [tiempo,setTiempo] = useState("");
-    const [archivo,setArchivo] = useState(null);
+    const [entregada,setEntregada] = useState(false);
+    const [enviado,setEnviado] = useState(true);
+
+    const inicialCargado = "Arrastra y Suelta el archivo, o bien da click para buscar el archivo"
+
+    const [cargado,setCargado] = useState(inicialCargado);
+
+    const inicialArchivo = {
+        archivo:null,
+        archivoNombre:'',
+        archivoURL:''
+    }
+    const [archivo,setArchivo] = useState(inicialArchivo);
+
     const changeFile = (e) => {
-        console.log(e.target.files[0]);
+        console.log(e.target.files[0].name);
         if (e.target.files[0] !== undefined) {
-            //ENVIAR ARCHIVO
+            setArchivo({
+                archivo:e.target.files[0],
+                archivoNombre:e.target.files[0].name
+            })
+            setCargado(`Se ha cargado el archivo: ${e.target.files[0].name}`)
         }
     };
 
@@ -31,22 +51,34 @@ const Entregar =()=>{
 
     //TODOS LOS DATOS DE ESTE USEEFFECT DEBEN PROVEERLOS LA PAGINA ANTERIOR
     useEffect(() => {
-        
+        const existTarea = async(e)=>{
+            const data = await privateAxios.get('/api/v1/tareas/entregada',{
+                params:{
+                    idclas:idClase, 
+                    idest:idEstudiante,
+                    num:numTarea
+                }
+            }).then((response)=>{console.log(response.data.rslt)
+                setEntregada(response.data.rslt)
+            })
+        }
+        existTarea()
+
         const loadTarea = async(e)=>{ 
             const data = await privateAxios.get(
-                '/api/v1/tareas/alltareas',{
+                '/api/v1/tareas/tareabyid',{
                     params : {
-                        id:id,
+                        idclas:idClase,
+                        num:numTarea
                     }
                 }
             ).then((response)=>{
                 const tarea = response.data.rslt[0];
-                //console.log(tarea)
+                console.log(tarea)
                 setTitulo(tarea.info.titulo)
                 setDescripcion(tarea.info.descripcion)
                 setPuntaje(tarea.info.Puntaje)
                 setTiempo(tiempoRestante(tarea.fechaEntrega))
-
 
             })
             .catch((response)=>{console.log(response)})
@@ -54,14 +86,101 @@ const Entregar =()=>{
         }
         loadTarea();
       
-    }, [])
+    }, [enviado])
 
-    const clickEntregar = async() =>{
+    const clickEntregar = async(e) =>{
+
+        e.preventDefault();
+        //SE CONFIGURA EL ARCHIVO DE LA TAREA
+        const fd = new FormData()
+        fd.append('file',archivo.archivo,archivo.archivoNombre);
+        var error = false;
         const tarea = await privateAxios.put("/api/v1/tareas/newentregable",{
+            params:{
+                idclas:"6225659f6208d12d1cdce801", 
+                idest:"622541aef1bd3418216d8831"
+            },
+            body:{
+                numeroTarea: numTarea, 
+                documentoURL:archivo.archivoNombre, 
+                estado:"true",
+                fechaEntrega:new Date(Date.now()).toISOString()
+            }
+        }).then((response)=>{
+            console.log(response)})
+        .catch((err)=>{
+            console.log(err)
+            error=true
+        });
+        
+        
+        // SE ENVIA LA TAREA
+        if(!error){
+            const fl = await privateAxios.post('/api/v1/tareas/file',fd,{
+                onUploadProgress: progressEvent =>{
+                    console.log('Upload progress: '+Math.round(progressEvent.loaded/progressEvent.total *100)+'%')
+                }
+            })
+            setArchivo(inicialArchivo);
+            setCargado(inicialCargado)
+            console.log(archivo)
 
-        })
+        }
+        setEnviado(!enviado)
+    }
+
+    const clickActualizar = async(e)=>{
+        e.preventDefault();
+        //SE CONFIGURA EL ARCHIVO DE LA TAREA
+        const fd = new FormData()
+        fd.append('file',archivo.archivo,archivo.archivoNombre);
+        var error = false;
+        const tarea = await privateAxios.put("/api/v1/tareas/updateentregable",{
+            params:{
+                idclas:"6225659f6208d12d1cdce801", 
+                idest:"622541aef1bd3418216d8831"
+            },
+            body:{
+                numeroTarea: numTarea, 
+                documentoURL:archivo.archivoNombre, 
+                estado:"true",
+                fechaEntrega:new Date(Date.now()).toISOString()
+            }
+        }).then((response)=>{
+            console.log(response)})
+        .catch((err)=>{
+            console.log(err)
+            error=true
+        });
+        
+        
+        // SE ENVIA LA TAREA
+        if(!error){
+            const fl = await privateAxios.post('/api/v1/tareas/file',fd,{
+                onUploadProgress: progressEvent =>{
+                    console.log('Upload progress: '+Math.round(progressEvent.loaded/progressEvent.total *100)+'%')
+                }
+            })
+            setArchivo(inicialArchivo);
+            setCargado(inicialCargado)
+            console.log(archivo)
+
+        }
+        setEnviado(!enviado)
     }
     
+    const clickEliminar =async(e)=>{
+        e.preventDefault();
+        const del  =  await privateAxios.delete('/api/v1/tareas/deleteentregable',{
+            params:{
+                idclas:"6225659f6208d12d1cdce801", 
+                idest:"622541aef1bd3418216d8831",
+                numeroTarea:numTarea
+            }
+        }).then((res)=>{console.log(res)})
+        .catch((err)=>{console.log(err)})
+        setEnviado(!enviado)
+    }
     
 
     return(
@@ -87,6 +206,9 @@ const Entregar =()=>{
                             <p className="tiempo-restante">
                                 {tiempo}
                             </p>
+                            <br></br>
+                            <h3 className="subtitulo">Estado</h3> 
+                            <p className={entregada?"entregado":"pendiente"}>{entregada?"Entregado":"Sin Entregar"}</p>
                         </div>
 
 
@@ -101,6 +223,8 @@ const Entregar =()=>{
                             <div className="image-upload-wrap">
                                 <input
                                     className="file-upload-input"
+                                    id="archivo-input"
+                                    name="archivo"
                                     type="file"
                                     accept="application/pdf"
                                     multiple
@@ -109,17 +233,32 @@ const Entregar =()=>{
                                     }}
                                 />
                                 <div className="text-information">
-                                    <h3>Arrastra y Suelta el archivo, o bien da click para buscar el archivo</h3>
+                                    <h3>{cargado}</h3>
                                 </div>
                             </div>
                             <div className="btn-group">
                                 <button className="btn-cancel">Cancelar</button>
                                 <button className="btn-up"
-                                    // onClick={(e)=>{
-                                    //     // getTareas(e);
-                                    // }}
+                                    style={entregada?{display:"none"}:{}}
+                                    onClick={(e)=>{
+                                        clickEntregar(e)
+                                    }}
                                 >Enviar Tarea</button>
-                                
+
+                                <button className="btn-update"
+                                    style={!entregada?{display:"none"}:{}}
+                                    onClick={(e)=>{
+                                        clickActualizar(e)
+                                    }}
+                                >Actualizar Tarea
+                                </button>
+
+                                <button className="btn-del"
+                                    style={!entregada?{display:"none"}:{}}
+                                    onClick={(e)=>{
+                                        clickEliminar(e);
+                                    }}
+                                >Eliminar Tarea</button>
                             </div>
                             <br></br>
                         </div>
