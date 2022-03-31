c4927ab1a41ff1f00836f218703829ea1d726709
manrique-code <sergio.mnrq1@gmail.com>
2022-03-31 01:45:51 -0600

    Añadidos archivos de diseño
    Pendiente:
    * Interfaz de Login
    * Interfaz de Sigin
    
    Nota:
    * Aquí se detalla la estructura de diseño que se van a tomar con los
      archivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/ui/buttons/Button";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button
+      icon={faArrowRight}
+      label="Hola mundo!"
+      size="lg"
+      title="Hola desde el hover"
+      type="primary"
+    />
   );
 }
 
