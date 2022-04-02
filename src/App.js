50bc57bcec748c3ff1c41256a3542b81dd2b9b71
manrique-code <sergio.mnrq1@gmail.com>
2022-04-01 23:23:43 -0600

    Implementando Routes
    Añadido:
    * Rutas de navegación entre componentes de pantalla
    * Provider de reducers de la interfaz
    * Hoja de estilos con la fuente personalizada de la página
    
    Pendiente:
    * Añadir todas las pantallas de la PWA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import Entregar from "./components/screens/Tareas/EntregarTareas/EntregarTareas";
+import "./assets/css/fonts.css";
+
+import store from "./store/index";
+import Login from "./components/screens/LoginSignIn/LoginSignIn";
 
 function App() {
-  return <Entregar></Entregar>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
