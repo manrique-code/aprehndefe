fa1a06de7b604b177c0670f14fa6bca1ecd42699
manrique-code <sergio.mnrq1@gmail.com>
2022-04-01 23:28:58 -0600

    Añadida la pantalla de Entregar Tareas
    Añadido:
    * Pantalla de Entregar Tareas dentro de una ruta
    
    Pendiente:
    * El path puede cambiar según conveniencia

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import Entregar from "./components/screens/Tareas/EntregarTareas/EntregarTareas.js";
 import "./assets/css/fonts.css";
 
 import store from "./store/index";
+import Entregar from "./components/screens/Tareas/EntregarTareas/EntregarTareas.js";
 import Login from "./components/screens/LoginSignIn/LoginSignIn";
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/tareas/entregar" element={<Entregar />} />
         </Routes>
       </BrowserRouter>
     </Provider>
