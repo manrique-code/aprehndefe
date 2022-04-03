f2b73b9bad7c64d762e489238c57bd2bbf1d21f8
manrique-code <sergio.mnrq1@gmail.com>
2022-04-03 17:25:55 -0600

    Añadida pantalla de Matricular Clase
    Añadido:
    * Pantalla en donde se ingresará el código de la clase a matricular
    
    Pendiente:
    * Añadir estilos a la misma

---
+++ src/components/screens/MatricularClase/MatricularClase.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./MatricularClase.css";
+import MatricularClaseForm from "../../ui/forms/matricularClaseForm/MatricularClaseForm";
+
+const MatricularClase = () => {
+  return <MatricularClaseForm />;
+};
+
+export default MatricularClase;
